a0e6473accb0418e36e336ffac6355737959ad79
Shimpali Deshpande <shimpali.deshpande@yahoo.com>
2022-03-15 09:37:06 +0530

    Setup fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,5 +11,5 @@ export const environment = {
   version: env['npm_package_version'],
   serverUrl: 'https://api.chucknorris.io',
   defaultLanguage: 'en-US',
-  supportedLanguages: ['en-US', 'es-ES', 'fr-FR']
+  supportedLanguages: ['en-US', 'es-ES', 'fr-FR'],
 };
