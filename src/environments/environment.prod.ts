12aa6af9e407e9d55e17bd1ec36120bcffb99b79
Shimpali Deshpande <shimpali.deshpande@yahoo.com>
2022-06-02 17:17:34 +0530

    Fixed build error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ export const environment = {
   serverUrl: 'https://api.chucknorris.io',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'es-ES', 'fr-FR'],
+  googleBooksAPIUrl: 'https://www.googleapis.com/books/v1/volumes',
 };
